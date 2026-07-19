6771393eb223eb7a8be1f48c1ff0690371c01f25
PiUnknown <okjha09@gmail.com>
2026-07-20 00:09:40 +0530

    Add Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(<App />);
